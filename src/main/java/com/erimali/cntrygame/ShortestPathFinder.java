658f95036dc8f062dcb72c7f554bd0c8481db516
Ermal Arapi <arjonermal@gmail.com>
2024-05-09 14:20:23 +0200

    Upload

--- src/main/java/com/erimali/cntrygame/ShortestPathFinder.java
+++ src/main/java/com/erimali/cntrygame/ShortestPathFinder.java
@@ -12,10 +12,12 @@ public class ShortestPathFinder {
     public ShortestPathFinder(SVGProvince[] svgProvinces) {
         this.svgProvinces = svgProvinces;
         provinceData = generateNeighbourMap();
+        fixBiDirectionalGraphMap(provinceData);
     }
 
     public ShortestPathFinder(Map<Integer, int[]> provinceData) {
         this.provinceData = provinceData;
+        fixBiDirectionalGraphMap(provinceData);
     }
 
     public List<Integer> findShortestPath(int source, int destination) {
@@ -93,6 +95,36 @@ public class ShortestPathFinder {
             }
         }
         return map;
+    }
+
+    public static void fixBiDirectionalGraphMap(Map<Integer, int[]> map) {
+        Map<Integer, List<Integer>> newValues = new HashMap<>();
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            for (int i : entry.getValue()) {
+                if (!map.containsKey(i) && !newValues.containsKey(i)) {
+                    List<Integer> list = new LinkedList<>();
+                    list.add(entry.getKey());
+                    newValues.put(i, list);
+                } else if (newValues.containsKey(i)) {
+                    List<Integer> list = newValues.get(i);
+                    list.add(entry.getKey());
+                }
+            }
+        }
+        for (Map.Entry<Integer, List<Integer>> entry : newValues.entrySet()) {
+            map.put(entry.getKey(), listToBasicArr(entry.getValue()));
+        }
+
+    }
+
+    public static int[] listToBasicArr(List<Integer> list) {
+        int[] arr = new int[list.size()];
+        //faster for linked lists
+        Iterator<Integer> iterator = list.iterator();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = iterator.next();
+        }
+        return arr;
     }
 
     public static Map<Integer, int[]> generateNeighbourMap() {
